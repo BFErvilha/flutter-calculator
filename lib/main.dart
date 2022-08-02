2f759445de8672b23f18817148acf4c8901ddc3f
Bruno Ervilha <bruno.ervilha@landapp.com.br>
2022-08-02 13:53:11 -0300

    calc plus function

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,95 @@ class MyApp extends StatefulWidget {
 
 
 class _MyAppState extends State<MyApp> {
+
+  String result = '';
+  double number1 = 0.0;
+  double number2 = 0.0;
+
   @override
   Widget build(BuildContext context) {
-    TextField num1 = const TextField(
+    TextField num1 = TextField(
       keyboardType: TextInputType.number,
+      style: TextStyle(
+        color: Colors.deepOrange
+      ),
+      decoration: InputDecoration(
+        labelText: 'Número 01',
+        labelStyle: TextStyle(
+            color: Colors.deepOrange
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.deepOrange, width: 1.5)
+        ),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepOrange, width: 1.5)
+        ),
+      ),
+      onChanged: (value) {
+        number1 = double.parse(value);
+      },
     );
 
-    TextField num2 = const TextField(
+    TextField num2 = TextField(
       keyboardType: TextInputType.number,
+      style: TextStyle(
+          color: Colors.deepOrange
+      ),
+      decoration: InputDecoration(
+        labelText: 'Número 02',
+        labelStyle: TextStyle(
+            color: Colors.deepOrange
+        ),
+        enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepOrange, width: 1.5)
+        ),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepOrange, width: 1.5)
+        ),
+      ),
+      onChanged: (value) {
+        number2 = double.parse(value);
+      }
     );
 
     RaisedButton button = RaisedButton(
-      onPressed: null,
       child: Text('Calcular'),
+      color: Colors.deepOrange,
+      textColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16)
+      ),
+      onPressed: () {
+        setState(() {
+          double calc = number1 + number2;
+          this.result = "O resultado da soma é $calc";
+        });
+      },
+    );
+
+    Text result = Text(
+      this.result,
+      style: TextStyle(
+        color: Colors.deepOrange
+      ),
+    );
+
+    Padding separator = Padding(
+        padding: EdgeInsets.all(4.0),
     );
 
     Column columns = Column(
       children: <Widget>[
         num1,
+        separator,
         num2,
-        button
+        separator,
+        SizedBox(
+          child: button,
+          width: double.infinity,
+        ),
+        separator,
+        result
       ],
     );
 
@@ -39,6 +108,7 @@ class _MyAppState extends State<MyApp> {
           appBar: AppBar(
             title: Text('Calculator'),
             centerTitle: true,
+            backgroundColor: Colors.deepOrange,
           ),
           body: Padding(
             padding: EdgeInsets.all(15.0),
